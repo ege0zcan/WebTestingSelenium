df09aeb859ce924cb65611f56f7cb48851344840
Yusuf Dalva <yusuf.dalva.1@gmail.com>
2020-02-24 17:47:32 +0300

    Update config.php

--- webpage/src/config.php
+++ webpage/src/config.php
@@ -7,9 +7,9 @@
 
 	$mysqli = mysqli_connect($host_name, $username, $password, $db_name);
 	if (!$mysqli) {
-    	echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    	exit;
+    		echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    		echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    		exit;
 	}
 ?>
